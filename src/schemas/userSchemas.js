79cfc197c76a50287b83a4d055322c7f0eb132b2
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-17 11:39:43 -0300

    feat: ensure password and passwordConfiramtion are the same

--- src/schemas/userSchemas.js
+++ src/schemas/userSchemas.js
@@ -1,25 +1,35 @@
 import z from "zod";
 
-export const signupSchema = z.object({
-  first_name: z.string().trim().min(1, {
-    message: "O nome é obrigatório",
-  }),
-  last_name: z.string().trim().min(1, {
-    message: "O sobrenome é obrigatório",
-  }),
-  email: z.email({
-    message: "O email é inválido",
-  }),
-  password: z.string().trim().min(6, {
-    message: "A senha deve ter no mínimo 6 caractéres",
-  }),
-  passwordConfirmation: z.string().trim().min(6, {
-    message: "A confirmação de senha é obrigatória",
-  }),
-  terms: z.boolean().refine((value) => value === true, {
-    message: "Você precisa aceitar os termos",
-  }),
-});
+export const signupSchema = z
+  .object({
+    first_name: z.string().trim().min(1, {
+      message: "O nome é obrigatório",
+    }),
+    last_name: z.string().trim().min(1, {
+      message: "O sobrenome é obrigatório",
+    }),
+    email: z.email({
+      message: "O email é inválido",
+    }),
+    password: z.string().trim().min(6, {
+      message: "A senha deve ter no mínimo 6 caractéres",
+    }),
+    passwordConfirmation: z.string().trim().min(6, {
+      message: "A confirmação de senha é obrigatória",
+    }),
+    terms: z.boolean().refine((value) => value === true, {
+      message: "Você precisa aceitar os termos",
+    }),
+  })
+  .refine(
+    (data) => {
+      return data.password === data.passwordConfirmation;
+    },
+    {
+      message: "As senhas não coinciden",
+      path: ["passwordConfirmation"],
+    }
+  );
 
 export const loginSchema = z.object({
   email: z.email({ message: "o email é inválido" }),
