728f57cf72585d9ec35a8a5968201f73d137384d
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-17 10:12:42 -0300

    chore: add react query setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,21 @@ import HomePage from "./pages/Home";
 import SignupPage from "./pages/signup";
 import LoginPage from "./pages/login";
 import NotFoundPage from "./pages/notFound";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
