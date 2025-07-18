d15ffbb24eea0047601386bc59f148c26b16a373
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-18 18:36:28 -0300

    feat: add axios interceptor to atatch the access token in every request

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,18 @@
+import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from "@/constants/localstorage";
 import axios from "axios";
 
 export const api = axios.create({
   baseURL: "http://localhost:3000",
+});
+
+// é como se fosse um middlleware que passa pelas rotas e coloca o access token no authrization
+api.interceptors.request.use((request) => {
+  const accessToken = localStorage.getItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY);
+
+  if (!accessToken) {
+    return request;
+  }
+
+  request.headers.Authorization = `Bearer ${accessToken}`;
+  return request;
 });
