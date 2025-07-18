b880b144ec5554d21bf7b59bff58bc024e2121d6
carlosEA28 <carloseaduarte28@gmail.com>
2025-07-18 19:04:27 -0300

    feat: ensure refreshToken route is called when request retrives a 401 error

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,4 +1,7 @@
-import { LOCAL_STORAGE_ACCESS_TOKEN_KEY } from "@/constants/localstorage";
+import {
+  LOCAL_STORAGE_ACCESS_TOKEN_KEY,
+  LOCAL_STORAGE_REFRESH_TOKEN_KEY,
+} from "@/constants/localstorage";
 import axios from "axios";
 
 export const api = axios.create({
@@ -21,3 +24,44 @@ api.interceptors.request.use((request) => {
   request.headers.Authorization = `Bearer ${accessToken}`;
   return request;
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const request = error.config;
+
+    //verificar se eu tenho ym refresh Token
+    const refreshToken = localStorage.getItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY);
+    if (!refreshToken) {
+      return Promise.reject(error);
+    }
+
+    //verficar se o erro e 401
+    if (
+      error.response.status === 401 &&
+      !request._retry &&
+      !request.url.includes("/api/auth/refredh-token")
+    ) {
+      request._retry = true;
+      try {
+        // se eu tiver um refresh token, eu tenho que atualizar o meu access token e refazer a requisicao
+        const response = await api.post("/api/users/refresh-token", {
+          refreshToken,
+        });
+
+        const newAccessToken = response.data.accessToken;
+        const newRefreshToken = response.data.refreshToken;
+        localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, newAccessToken);
+        localStorage.setItem(LOCAL_STORAGE_REFRESH_TOKEN_KEY, newRefreshToken);
+        request.headers.Authorization = `Bearer ${newAccessToken}`;
+        return api(request);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
